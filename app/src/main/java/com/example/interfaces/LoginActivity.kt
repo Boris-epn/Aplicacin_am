310d6ed212fee54bdd9b9aa630d0ac8a9f1077a6
Boris Gabriel Garcés Proaño <boris.garces@epn.edu.ec>
2026-07-08 18:02:55 -0500

    Guardar datos de usuario

--- app/src/main/java/com/example/interfaces/LoginActivity.kt
+++ app/src/main/java/com/example/interfaces/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.interfaces
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -13,11 +14,18 @@ class LoginActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_login)
 
+        // SharedPreferences
+        val sharedPreferences = getSharedPreferences("LoginPrefs", Context.MODE_PRIVATE)
+
         // Recupero valores ...
         val mail = findViewById<EditText>(R.id.txt_mail)
         val pass = findViewById<EditText>(R.id.txt_pass)
         val login = findViewById<Button>(R.id.btn_login)
         val signUp = findViewById<Button>(R.id.btn_signUp)
+
+        // Cargar usuario guardado
+        val savedMail = sharedPreferences.getString("user_mail", "")
+        mail.setText(savedMail)
 
         // Evento para el botón ...
 //        login.setOnClickListener {
@@ -30,8 +38,15 @@ class LoginActivity : AppCompatActivity() {
 //        }
         // BOTON LOGIN
         login.setOnClickListener {
+            val userMail = mail.text.toString()
+
+            // Guardar en SharedPreferences
+            val editor = sharedPreferences.edit()
+            editor.putString("user_mail", userMail)
+            editor.apply()
+
             val intent = Intent(this, HomeActivity::class.java)
-            intent.putExtra("clave", mail.text.toString())
+            intent.putExtra("clave", userMail)
             startActivity(intent)
         }
 
